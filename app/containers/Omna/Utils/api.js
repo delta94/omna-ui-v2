ca9704f763487d7de4cf1b1b39960d97a4e5c9ab
Hansel Bormey <hanselbormey@gmail.com>
2019-08-02 16:32:28 -0400

    Fix post methods

--- app/containers/Omna/Utils/api.js
+++ app/containers/Omna/Utils/api.js
@@ -4,12 +4,22 @@ import get from 'lodash/get';
 
 function setParams(config) {
   const params = get(config, 'params', {});
+  const data = get(config, 'data', {});
   const currentTenant = JSON.parse(sessionStorage.getItem('currentTenant'));
+  if (config.method === 'post') {
+    data.token = currentTenant.token;
+    data.timestamp = Date.now();
+
+    const msg = config.url + JSON.stringify(data).replace(/["']/g, '').split('').sort()
+      .join('');
+
+    // Generate the corresponding hmac using the js-sha256 or similar library.
+    data.hmac = sha256.hmac.update(currentTenant.secret, msg).hex();
+    return Object.assign(config, { data });
+  }
   params.token = currentTenant.token;
   params.timestamp = Date.now();
-  /*   if (config.data) {
-    params.data = { ...config.data.data };
-  } */
+
   // Join the service path and the ordered sequence of characters, excluding the quotes,
   // corresponding to the JSON of the parameters that will be sent.
   const msg = config.url + JSON.stringify(params).replace(/["']/g, '').split('').sort()
