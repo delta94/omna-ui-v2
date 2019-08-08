69ab227140c301a3b8db1532f6f1efcc9ec252b4
alegd <ale.gueden@gmail.com>
2019-08-08 16:43:45 -0400

    Add pagination to Integrations view

--- app/containers/Omna/Settings/Integrations/Integrations.js
+++ app/containers/Omna/Settings/Integrations/Integrations.js
@@ -1,32 +1,35 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { withSnackbar } from 'notistack';
+import get from 'lodash/get';
 
 // material-ui
 import { withStyles } from '@material-ui/core/styles';
+import Divider from '@material-ui/core/Divider';
+import Button from '@material-ui/core/Button';
+import Paper from '@material-ui/core/Paper';
+import Table from '@material-ui/core/Table';
+import TableRow from '@material-ui/core/TableRow';
+import TableFooter from '@material-ui/core/TableFooter';
+import TablePagination from '@material-ui/core/TablePagination';
+
 import Avatar from '@material-ui/core/Avatar';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardHeader from '@material-ui/core/CardHeader';
 import Tooltip from '@material-ui/core/Tooltip';
-import Divider from '@material-ui/core/Divider';
-import Button from '@material-ui/core/Button';
-import Paper from '@material-ui/core/Paper';
-
 import IconButton from '@material-ui/core/IconButton';
 import DeleteIcon from '@material-ui/icons/Delete';
 import VerifiedUserIcon from '@material-ui/icons/VerifiedUser';
 import BlockIcon from '@material-ui/icons/Block';
 
-import { withSnackbar } from 'notistack';
-
-import PropTypes from 'prop-types';
-import API from '../../Utils/api';
-import Utils from '../../Common/Utils';
 import LoadingState from '../../Common/LoadingState';
 import AlertDialog from '../../Common/AlertDialog';
-
+import Utils from '../../Common/Utils';
+import GenericTablePagination from '../../Common/GenericTablePagination';
+import API from '../../Utils/api';
 
 const styles = theme => ({
-
   cardList: {
     display: 'flex',
     flexWrap: 'wrap',
@@ -37,93 +40,108 @@ const styles = theme => ({
     margin: 5
   },
   actions: {
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
   title: {
-    fontSize: 14,
+    fontSize: 14
   },
   pos: {
-    marginBottom: 2,
+    marginBottom: 2
   },
   background: {
     backgroundColor: theme.palette.background.paper
   }
-
 });
 
 class Integrations extends Component {
   state = {
     loading: true,
-    integrations: [],
+    integrations: { data: [], pagination: {} },
     alertDialog: {
       open: false,
       integrationId: '',
       integrationName: '',
       message: ''
-    }
-  }
+    },
+    limit: 5,
+    page: 0
+  };
 
   componentDidMount() {
     this.getIntegrations();
   }
 
-  getIntegrations = () => {
+  getIntegrations = params => {
     this.setState({ loading: true });
     const params = { with_details: true };
-    API.get('/integrations', { params }).then(response => {
-      this.setState({ integrations: response.data.data });
-    }).catch((error) => {
-      // handle error
-      console.log(error);
-    }).then(() => {
-      this.setState({ loading: false });
-    });
-  }
+    API.get('/integrations', { params })
+      .then(response => {
+        this.setState({
+          integrations: get(response, 'data', { data: [], pagination: {} }),
+          limit: get(response, 'data.pagination.limit', 0)
+        });
+      })
+      .catch(error => {
+        // handle error
+        console.log(error);
+      })
+      .then(() => {
+        this.setState({ loading: false });
+      });
+  };
 
-  deleteIntegration = (id) => {
+  deleteIntegration = id => {
     const { enqueueSnackbar } = this.props;
     this.setState({ loading: true });
-    API.get(`/integrations/${id}/destroy`).then(() => {
-      enqueueSnackbar('Integration deleted successfully', {
-        variant: 'success'
+    API.get(`/integrations/${id}/destroy`)
+      .then(() => {
+        enqueueSnackbar('Integration deleted successfully', {
+          variant: 'success'
+        });
+      })
+      .catch(error => {
+        enqueueSnackbar(error, {
+          variant: 'error'
+        });
+      })
+      .then(() => {
+        this.setState({ loading: false });
       });
-    }).catch((error) => {
-      enqueueSnackbar(error, {
-        variant: 'error'
-      });
-    }).then(() => {
-      this.setState({ loading: false });
-    });
-  }
+  };
 
   handleAddIntegrationClick = () => {
     const { history } = this.props;
     history.push('/app/settings/integrations/add-integration');
-  }
+  };
 
-  handleAuthorization = (id) => {
-    window.location.replace(`${Utils.baseAPIURL()}/integrations/${id}/authorize?redirect_id=${Utils.returnAfterAuthorization()}`);
-  }
+  handleAuthorization = id => {
+    window.location.replace(
+      `${Utils.baseAPIURL()}/integrations/${id}/authorize?redirect_id=${Utils.returnAfterAuthorization()}`
+    );
+  };
 
-  handleUnAuthorization = (id) => {
+  handleUnAuthorization = id => {
     const { enqueueSnackbar } = this.props;
     this.setState({ loading: true });
-    API.get(`/integrations/${id}/unauthorize`).then(() => {
-      enqueueSnackbar('Integration unauthorized successfully', {
-        variant: 'success'
+    API.get(`/integrations/${id}/unauthorize`)
+      .then(() => {
+        enqueueSnackbar('Integration unauthorized successfully', {
+          variant: 'success'
+        });
+      })
+      .catch(error => {
+        // handle error
+        console.log(error);
+      })
+      .then(() => {
+        this.setState({ loading: false });
+        this.getIntegrations();
       });
-    }).catch((error) => {
-      // handle error
-      console.log(error);
-    }).then(() => {
-      this.setState({ loading: false });
-      this.getIntegrations();
-    });
-  }
+  };
 
   handleDialogCancel = () => {
     this.setState({ alertDialog: false });
-  }
+  };
 
   handleDialogConfirm = async () => {
     const { enqueueSnackbar } = this.props;
@@ -137,13 +155,15 @@ class Integrations extends Component {
       }
       this.getIntegrations();
     } catch (error) {
-      const errorMessage = error ? error.response.data.message : 'unknown Error';
+      const errorMessage = error
+        ? error.response.data.message
+        : 'unknown Error';
       enqueueSnackbar(errorMessage, {
         variant: 'error'
       });
     }
     this.setState({ alertDialog: false });
-  }
+  };
 
   handleDeleteClick = (id, name) => {
     this.setState({
@@ -154,11 +174,28 @@ class Integrations extends Component {
         message: `Are you sure you want to remove "${name}" integration?`
       }
     });
-  }
+  };
+
+  handleChangePage = (e, page) => {
+    const { limit } = this.state;
+    this.setState({ page });
+
+    const params = {
+      offset: page * limit,
+      limit
+    };
+
+    this.getIntegrations(params);
+  };
 
   render() {
     const { classes } = this.props;
-    const { integrations, loading, alertDialog } = this.state;
+    const {
+      integrations, loading, alertDialog, limit, page
+    } = this.state;
+
+    const { pagination, data } = integrations;
+    const count = get(pagination, 'total', 0);
 
     return (
       <div>
@@ -176,42 +213,82 @@ class Integrations extends Component {
           </div>
           <Divider variant="middle" />
           <div className={classes.cardList}>
-            {integrations && integrations.map(({
-              id, name, channel, authorized, logo = Utils.urlLogo(channel)
-            }) => (
-              <Card className={classes.card} key={name}>
-                <CardHeader
-                  avatar={logo
-                    ? <Avatar src={logo} alt="logo" aria-label="Recipe" className={classes.avatar} /> : null
-                  }
-                  title={name}
-                  subheader={Utils.fullChannelName(channel)}
-                />
-                <CardActions className={classes.actions}>
-                  {authorized ? (
-                    <Tooltip title="unauthorize">
-                      <IconButton aria-label="unauthorize" onClick={() => this.handleUnAuthorization(id)}>
-                        <BlockIcon />
-                      </IconButton>
-                    </Tooltip>
-                  )
-                    : (
-                      <Tooltip title="authorize">
-                        <IconButton aria-label="authorize" onClick={() => this.handleAuthorization(id)}>
-                          <VerifiedUserIcon />
+            {data
+              && data.map(
+                ({
+                  id,
+                  name,
+                  channel,
+                  logo = Utils.urlLogo(channel),
+                  authorized
+                }) => (
+                  <Card className={classes.card} key={name}>
+                    <CardHeader
+                      avatar={
+                        logo ? (
+                          <Avatar
+                            src={logo}
+                            alt="logo"
+                            aria-label="Recipe"
+                            className={classes.avatar}
+                          />
+                        ) : null
+                      }
+                      title={name}
+                      subheader={Utils.fullChannelName(channel)}
+                    />
+                    <CardActions className={classes.actions}>
+                      {authorized ? (
+                        <Tooltip title="unauthorize">
+                          <IconButton
+                            aria-label="unauthorize"
+                            onClick={() => this.handleUnAuthorization(id)}
+                          >
+                            <BlockIcon />
+                          </IconButton>
+                        </Tooltip>
+                      ) : (
+                        <Tooltip title="authorize">
+                          <IconButton
+                            aria-label="authorize"
+                            onClick={() => this.handleAuthorization(id)}
+                          >
+                            <VerifiedUserIcon />
+                          </IconButton>
+                        </Tooltip>
+                      )}
+                      <Tooltip title="delete">
+                        <IconButton
+                          aria-label="delete"
+                          onClick={() => this.handleDeleteClick(id, name)}
+                        >
+                          <DeleteIcon />
                         </IconButton>
                       </Tooltip>
-                    )
-                  }
-                  <Tooltip title="delete">
-                    <IconButton aria-label="delete" onClick={() => this.handleDeleteClick(id, name)}>
-                      <DeleteIcon />
-                    </IconButton>
-                  </Tooltip>
-                </CardActions>
-              </Card>
-            ))}
+                    </CardActions>
+                  </Card>
+                )
+              )}
           </div>
+          <Table>
+            <TableFooter>
+              <TableRow>
+                <TablePagination
+                  colSpan={5}
+                  rowsPerPageOptions={[5, 10, 25, 50]}
+                  count={count}
+                  rowsPerPage={limit}
+                  page={page}
+                  SelectProps={{
+                    native: true
+                  }}
+                  onChangePage={this.handleChangePage}
+                  onChangeRowsPerPage={this.handleChangeRowsPerPage}
+                  ActionsComponent={GenericTablePagination}
+                />
+              </TableRow>
+            </TableFooter>
+          </Table>
         </Paper>
         <AlertDialog
           open={alertDialog.open}
@@ -219,7 +296,6 @@ class Integrations extends Component {
           handleCancel={this.handleDialogCancel}
           handleConfirm={this.handleDialogConfirm}
         />
-
       </div>
     );
   }
