e7f3f443e02541369a564ee302d3b46cae4ef807
alegd <ale.gueden@gmail.com>
2019-11-05 14:38:55 -0500

    make loading state more "visible"

--- app/components/Loading/index.js
+++ app/components/Loading/index.js
@@ -3,20 +3,38 @@ import { PropTypes } from 'prop-types';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import { withStyles } from '@material-ui/core/styles';
 const styles = {
+  root: {
+    zIndex: 1400,
+    width: '100%',
+    height: '100%',
+   top: 0,
+   left: 0,
+    position: 'fixed',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)'
+  },
   circularProgress: {
     zIndex: 1500,
     position: 'fixed',
     top: 'calc(50% - 45px)',
-    left: 'calc(50% - 45px)',
+    left: 'calc(50% - 45px)'
   }
 };
 
 function Loading(props) {
   const { classes } = props;
-  return (<CircularProgress className={classes.circularProgress} size={90} thickness={1} color="secondary" />);
+  return (
+    <div className={classes.root}>
+      <CircularProgress
+        className={classes.circularProgress}
+        size={70}
+        thickness={2}
+        color="secondary"
+      />
+    </div>
+  );
 }
 
 Loading.propTypes = {
-  classes: PropTypes.object.isRequired,
+  classes: PropTypes.object.isRequired
 };
-export default (withStyles(styles)(Loading));
+export default withStyles(styles)(Loading);
