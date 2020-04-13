e6078d9fa51b166e46760da71aa1cf021b721770
Hansel Bormey <hanselbormey@gmail.com>
2020-04-13 18:26:34 -0400

    Add publish and unpublish action

--- app/actions/productActions.js
+++ app/actions/productActions.js
@@ -1,45 +1,20 @@
 import * as types from './actionConstants';
 
-/* export const setProductProperties = (tabIndex, properties) => ({
-  type: types.SET_PRODUCT_PROPERTIES,
-  payload: { tabIndex, properties }
-}); */
-
 export const getProductVariantList = (integrationId, remoteProductId) => ({
   type: types.GET_PRODUCT_VARIANTS,
   payload: { integrationId, remoteProductId }
 });
 
-export const setProductName = (name) => ({
-  type: types.SET_PRODUCT_NAME,
-  name
+export const linkProduct = (productId, integrationIds, enqueueSnackbar) => ({
+  type: types.LINK_PRODUCT_ASYNC,
+  productId,
+  integrationIds,
+  enqueueSnackbar
 });
 
-export const setProductDescription = (description) => ({
-  type: types.SET_PRODUCT_DESCRIPTION,
-  description
+export const unLinkProduct = (productId ,integrationIds, enqueueSnackbar) => ({
+  type: types.UNLINK_PRODUCT_ASYNC,
+  productId,
+  integrationIds,
+  enqueueSnackbar
 });
-
-export const setProductPrice = (price) => ({
-  type: types.SET_PRODUCT_PRICE,
-  price
-});
-
-export const setProduct = (product) => ({
-  type: types.SET_PRODUCT,
-  product
-});
-
-export const setProductIntegrations = (integrations) => ({
-  type: types.SET_PRODUCT_INTEGRATIONS,
-  integrations
-});
-
-export const setProductProperties = (properties) => ({
-  type: types.SET_PRODUCT_PROPERTIES,
-  properties
-});
-
-/* export const getProductVariantList = () => (dispatch) => {
-  dispatch({ type: 'GET_PRODUCT_VARIANTS', payload: {} });
-}; */
