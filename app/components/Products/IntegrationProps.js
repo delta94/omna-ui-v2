81f108f3b4624c7193652d899d1e8ecc330569c6
Hansel Bormey <hanselbormey@gmail.com>
2020-08-15 21:39:23 -0400

    Modify UI in integrationProps component
    - Mount the content on a paper

--- app/components/Products/IntegrationProps.js
+++ app/components/Products/IntegrationProps.js
@@ -1,12 +1,22 @@
-import React, { useState, memo } from 'react';
+import React, {
+  useState, memo, Fragment, useEffect
+} from 'react';
 import PropTypes from 'prop-types';
-import Typography from '@material-ui/core/Typography';
 import Alert from 'dan-components/Notification/Alert';
+import { PapperBlock } from 'dan-components';
+import TypographySkeleton from 'dan-components/Skeleton/index';
 import { emptyArray } from 'dan-containers/Common/Utils';
 import FormBuilder from './FormBuilder';
 
-const IntegrationProps = memo(({ properties, errors = '', onTouchedProps }) => {
+const IntegrationProps = memo((props) => {
+  const {
+    properties, errors = '', title, description, loading, onTouchedProps
+  } = props;
   const [properties_, setProperties_] = useState(properties);
+
+  useEffect(() => {
+    setProperties_(properties);
+  }, [properties]);
 
   // handlePropertyChange to use in the future if the api makes changes on the properties
   /*   const handlePropertyChange = (e) => {
@@ -39,25 +49,32 @@ const IntegrationProps = memo(({ properties, errors = '', onTouchedProps }) => {
     }
   };
   return (
-    <div>
-      <Typography variant="subtitle2" gutterBottom>
-        Properties
-      </Typography>
-      {emptyArray(properties_) && errors ? (<Alert variant="error" message="Something wrong. No properties to show." />) : null}
-      {emptyArray(properties_) && !errors ? (<Alert variant="info" message="No properties to show." />) : null}
-      {properties_ && !errors && (<FormBuilder properties={properties_} onChange={handlePropertyChange} />)}
-    </div>
+    <PapperBlock title={title} icon="ios-card" desc={description}>
+      {loading ? <TypographySkeleton /> : (
+        <Fragment>
+          {emptyArray(properties_) && errors ? (<Alert variant="error" message="Something wrong. No properties to show." />) : null}
+          {emptyArray(properties_) && !errors ? (<Alert variant="info" message="No properties to show." />) : null}
+          {properties_ && !errors && (<FormBuilder properties={properties_} onChange={handlePropertyChange} />)}
+        </Fragment>
+      )}
+    </PapperBlock>
   );
 });
 
 IntegrationProps.propTypes = {
   properties: PropTypes.any.isRequired,
   errors: PropTypes.string,
+  title: PropTypes.string,
+  description: PropTypes.string,
+  loading: PropTypes.bool,
   onTouchedProps: PropTypes.func
 };
 
 IntegrationProps.defaultProps = {
   errors: '',
+  title: 'Properties',
+  description: '',
+  loading: false,
   onTouchedProps: () => { }
 };
 
