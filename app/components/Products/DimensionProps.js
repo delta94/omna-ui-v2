0e1a04ccf6e8014f2b421947033545619e9f39ea
Hansel Bormey <hanselbormey@gmail.com>
2020-08-19 19:21:48 -0400

    Change UI in Package props component

--- app/components/Products/DimensionProps.js
+++ app/components/Products/DimensionProps.js
@@ -1,13 +1,15 @@
 /* eslint-disable react/jsx-no-duplicate-props */
-import React, { memo } from 'react';
+import React, { memo, Fragment } from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
-
+import { PapperBlock } from 'dan-components';
+import Grid from '@material-ui/core/Grid';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
 
 import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
 import TextField from '@material-ui/core/TextField';
 import NumberFormatCustom from 'dan-components/NumberFormatCustom';
 
@@ -17,37 +19,28 @@ const styles = theme => ({
     flexWrap: 'wrap'
   },
   dimensionContainer: {
+    marginTop: theme.spacing(2),
+    padding: theme.spacing(1)
+  },
+  paperBlockContainer: {
     marginTop: theme.spacing(2)
   },
   title: {
-    margin: theme.spacing(1)
+    margin: theme.spacing(1, 1, 3, 0)
   },
   formControl: {
-    [theme.breakpoints.down('sm')]: {
-      width: '100%'
-    },
-    [theme.breakpoints.up('sm')]: {
-      width: '500px'
-    },
-    margin: theme.spacing(1)
+    width: '100%'
   },
 });
 
-const DimensionProps = memo((props) => {
+const Body = (props) => {
   const {
-    weight, height, length, width, content, overwrite = false, overwriteOption = false, classes, onDimensionChange
+    weight, height, length, width, content, overwrite = false, overwriteOption = false, classes,
+    onDimensionChange, onContentChange, onOverwriteChange
   } = props;
-
-  const handleOverwriteChange = (e) => onDimensionChange({ target: { name: e.target.name, value: e.target.checked } });
-
-  const handleContentChange = (e) => onDimensionChange({ target: { name: e.target.name, value: e.target.value } });
-
   return (
-    <Paper className={classes.dimensionContainer} elevation={1}>
-      <Typography className={classes.title} variant="subtitle2" gutterBottom>
-        Package
-      </Typography>
-      <div className={classes.dimensionItems}>
+    <Grid container spacing={3}>
+      <Grid item xs={12} sm={6}>
         <TextField
           className={classes.formControl}
           label="Weight"
@@ -63,6 +56,8 @@ const DimensionProps = memo((props) => {
             inputComponent: NumberFormatCustom,
           }}
         />
+      </Grid>
+      <Grid item xs={12} sm={6}>
         <TextField
           className={classes.formControl}
           label="Height"
@@ -78,6 +73,8 @@ const DimensionProps = memo((props) => {
             inputComponent: NumberFormatCustom,
           }}
         />
+      </Grid>
+      <Grid item xs={12} sm={6}>
         <TextField
           className={classes.formControl}
           label="Width"
@@ -93,6 +90,8 @@ const DimensionProps = memo((props) => {
             inputComponent: NumberFormatCustom,
           }}
         />
+      </Grid>
+      <Grid item xs={12} sm={6}>
         <TextField
           className={classes.formControl}
           label="Length"
@@ -108,15 +107,19 @@ const DimensionProps = memo((props) => {
             inputComponent: NumberFormatCustom,
           }}
         />
+      </Grid>
+      <Grid item xs={12} sm={6}>
         <TextField
           className={classes.formControl}
           label="Content"
           name="content"
           value={content}
-          onChange={handleContentChange}
+          onChange={onContentChange}
           variant="outlined"
           id="content-input"
         />
+      </Grid>
+      <Grid item xs={12} sm={6}>
         {overwriteOption && (
           <FormControlLabel
             className={classes.formControl}
@@ -126,7 +129,7 @@ const DimensionProps = memo((props) => {
                   id="overwrite-input"
                   name="overwrite"
                   checked={overwrite}
-                  onChange={handleOverwriteChange}
+                  onChange={onOverwriteChange}
                   value="overwrite"
                   color="default"
                 />
@@ -135,10 +138,90 @@ const DimensionProps = memo((props) => {
             label="overwrite package information in all variants"
           />
         )}
-      </div>
-    </Paper>
+      </Grid>
+    </Grid>
+  );
+};
+
+const DimensionProps = memo((props) => {
+  const {
+    weight, height, length, width, content, overwrite = false, overwriteOption = false, classes,
+    onDimensionChange, unmountPaper
+  } = props;
+
+  const handleOverwriteChange = (e) => onDimensionChange({ target: { name: e.target.name, value: e.target.checked } });
+
+  const handleContentChange = (e) => onDimensionChange({ target: { name: e.target.name, value: e.target.value } });
+
+  return (
+    <Fragment>
+      {!unmountPaper ? (
+        <div className={classes.paperBlockContainer}>
+          <PapperBlock title="Package" icon="ios-card" desc="Package dimensions and content">
+            <Body
+              weight={weight}
+              height={height}
+              length={length}
+              width={width}
+              content={content}
+              overwrite={overwrite}
+              overwriteOption={overwriteOption}
+              classes={classes}
+              onDimensionChange={onDimensionChange}
+              onContentChange={handleContentChange}
+              onOverwriteChange={handleOverwriteChange}
+            />
+          </PapperBlock>
+        </div>
+      ) : (
+        <Paper className={classes.dimensionContainer} elevation={1}>
+          <Typography className={classes.title} component="div" gutterBottom>
+            <Box fontSize="h6.fontSize" m={1} fontWeight="fontWeightRegular">
+              Package
+            </Box>
+          </Typography>
+          <Body
+            weight={weight}
+            height={height}
+            length={length}
+            width={width}
+            content={content}
+            overwrite={overwrite}
+            overwriteOption={overwriteOption}
+            classes={classes}
+            onDimensionChange={onDimensionChange}
+            onContentChange={handleContentChange}
+            onOverwriteChange={handleOverwriteChange}
+          />
+        </Paper>
+      )}
+    </Fragment>
   );
 });
+
+Body.propTypes = {
+  weight: PropTypes.number,
+  height: PropTypes.number,
+  length: PropTypes.number,
+  width: PropTypes.number,
+  overwrite: PropTypes.bool,
+  overwriteOption: PropTypes.bool,
+  content: PropTypes.string,
+  classes: PropTypes.object.isRequired,
+  onContentChange: PropTypes.func.isRequired,
+  onOverwriteChange: PropTypes.func.isRequired,
+  onDimensionChange: PropTypes.func.isRequired
+};
+
+Body.defaultProps = {
+  weight: undefined,
+  height: undefined,
+  length: undefined,
+  width: undefined,
+  content: '',
+  overwrite: false,
+  overwriteOption: false,
+};
 
 DimensionProps.propTypes = {
   weight: PropTypes.number,
@@ -149,6 +232,7 @@ DimensionProps.propTypes = {
   overwriteOption: PropTypes.bool,
   content: PropTypes.string,
   classes: PropTypes.object.isRequired,
+  unmountPaper: PropTypes.bool,
   onDimensionChange: PropTypes.func.isRequired
 };
 
@@ -159,7 +243,8 @@ DimensionProps.defaultProps = {
   width: undefined,
   content: '',
   overwrite: false,
-  overwriteOption: false
+  overwriteOption: false,
+  unmountPaper: false
 };
 
 export default withStyles(styles)(DimensionProps);
